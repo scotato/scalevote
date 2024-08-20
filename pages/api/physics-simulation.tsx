7d0b0ee9c74dd2f9c3300388bff7a93b21f4ecfc
Scott Dodge <scott@scotato.com>
2024-08-20 15:29:48 -0700

    update endpoint

--- pages/api/physics-simulation.tsx
+++ pages/api/physics-simulation.tsx
@@ -5,7 +5,7 @@ import Game from "@/components/Game";
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const players = req.body.players as User[];
   const svgString = ReactDOMServer.renderToString(<Game players={players} />);
-  res.status(200).send(svgString);
+  res.status(200).setHeader("Content-Type", "text/plain").send(svgString);
 };
 
 export default handler;
